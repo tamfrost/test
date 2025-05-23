7de92bc7ffe406bf3328d3e4630d65f92b5a3d08
Johan Lundquist <johlun@lotus.fra>
2025-05-23 08:36:07 +0200

    feat: add environment variables to Docker action and log them in script

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('Hello World!');
+console.log(process.env.GITHUB_SHA);
+console.log(process.env.ACTIONS_RUNTIME_TOKEN);
+console.log(process.env.MY_ENV_VAR);
+console.log(process.env.MY_ENV_VAR2);
